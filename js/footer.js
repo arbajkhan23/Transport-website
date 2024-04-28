55d7d9b322ad24b698d131a793cafe63faae9110
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2024-04-28 14:24:58 +0530

    new work

---
+++ js/footer.js
@@ -0,0 +1,264 @@
+if (document.getElementById("footer")){
+    document.getElementById("footer").innerHTML = `
+    <div class="container">
+    <div class="row">
+      <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="py-4">
+          <img src="images/Logo.svg" alt="foot-logo" class="img-fluid" />
+          <p class="mb-0 text-white fs-6 fw-medium mt-5">
+            Leverage agile frameworks to provide a robust synopsis for
+            strategy collaborative thinking to further the overall value
+            proposition.
+          </p>
+          <div class="d-flex align-items-center mt-4">
+            <span
+              ><svg
+                width="64"
+                height="64"
+                viewBox="0 0 64 64"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="31.6309"
+                  cy="32"
+                  r="31"
+                  fill="#111C55"
+                  stroke="#273270"
+                />
+                <path
+                  d="M41.1268 23.5H23.1349C22.5804 23.5 22.1309 23.8755 22.1309 24.3387V37.6613C22.1309 38.1245 22.5804 38.5 23.1349 38.5H41.1268C41.6813 38.5 42.1309 38.1245 42.1309 37.6613V24.3387C42.1309 23.8755 41.6813 23.5 41.1268 23.5Z"
+                  stroke="#F6B426"
+                />
+                <path
+                  d="M22.1309 23.5L32.5556 30.5L42.1309 23.7294"
+                  stroke="#F6B426"
+                />
+              </svg>
+            </span>
+            <div class="ms-3">
+              <p class="text-white mb-0 fs-14">Email</p>
+              <p class="text-white mb-0 fs-14">contact@logistics.com</p>
+            </div>
+          </div>
+          <div class="d-flex align-items-center mt-3">
+            <span
+              ><svg
+                width="64"
+                height="63"
+                viewBox="0 0 64 63"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="31.6309"
+                  cy="31.5"
+                  r="31"
+                  fill="#111C55"
+                  stroke="#273270"
+                />
+                <path
+                  d="M24.1049 22.1408C23.9703 22.1449 23.8379 22.1743 23.7157 22.2272C23.5934 22.28 23.4837 22.3554 23.393 22.4487C23.3024 22.542 23.2326 22.6514 23.1879 22.7704C23.1432 22.8894 23.1245 23.0156 23.1328 23.1416C23.2673 25.3015 23.9013 30.5716 26.9368 33.8583C30.5718 37.8181 35.3057 39.1824 40.1626 38.9808C40.4229 38.9662 40.6676 38.8596 40.8474 38.6826C41.0271 38.5055 41.1284 38.2712 41.1309 38.0268V34.5674C41.1278 34.2446 41.0091 33.9322 40.7938 33.6802C40.5785 33.4282 40.2791 33.2513 39.9435 33.1779L37.5113 32.6739C37.2104 32.6138 36.8973 32.6397 36.6123 32.7481C36.3273 32.8566 36.0837 33.0427 35.9128 33.2823L35.3749 34.0454C35.3255 34.1153 35.2516 34.1668 35.1661 34.1911C35.0805 34.2154 34.9885 34.211 34.9061 34.1786C33.6688 33.6783 28.7389 31.5184 28.1971 27.8753C28.1865 27.805 28.1984 27.7333 28.2312 27.6692C28.264 27.6051 28.3163 27.5516 28.3816 27.5154L29.3576 26.9574C29.6253 26.802 29.8355 26.5732 29.9599 26.3022C30.0843 26.0311 30.1168 25.7308 30.053 25.4419L29.5189 23.1236C29.4409 22.7988 29.245 22.5093 28.9648 22.3045C28.6846 22.0998 28.3373 21.9923 27.982 22.0004L24.1049 22.1408Z"
+                  stroke="#F6B426"
+                />
+              </svg>
+            </span>
+            <div class="ms-3">
+              <p class="text-white mb-0 fs-14">Call Us</p>
+              <p class="text-white mb-0 fs-14">(00) 112 365 489</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-2 col-lg-2 col-md-2 col-sm-6 col-12">
+        <div class="py-4">
+          <h3 class="h3 fw-medium text-white mt-2 pb-3">Pages</h3>
+          <ul class="ps-0 mb-0 mt-4">
+            <li>
+              <a
+                href="Abouts.html"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >About Us</a
+              >
+            </li>
+            <li>
+              <a
+                href="Team.html"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Our Team</a
+              >
+            </li>
+            <li>
+              <a
+                href="Project Singl.html"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Our Project</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Pricing</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Contact</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-xl-2 col-lg-2 col-md-2 col-sm-6 col-12">
+        <div class="py-4">
+          <h3 class="h3 fw-medium text-white mt-2 pb-3">Utility</h3>
+          <ul class="ps-0 mb-0 mt-4">
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Style Guide</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Changelog</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Licenses</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Protected</a
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                class="fs-6 fw-medium text-white d-inline-block mb-3"
+                >Not Found</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="py-4">
+          <h3 class="h3 fw-medium text-white mt-2 pb-3">Subscribe</h3>
+          <form>
+            <input
+              type="email"
+              placeholder="Email here*"
+              class="w-100 p-3 bg-transparent border-solid border-1 text-white"
+              required
+            />
+            <div class="d-flex justify-content-between align-items-center">
+              <button
+                value="Submit"
+                class="mt-3 btn yellow-gra py-3 px-4 rounded-0"
+              >
+                Send Now
+              </button>
+              <ul class="ps-0 mb-0 d-flex gap-5 align-items-center">
+                <li>
+                  <a href="#"
+                    ><svg
+                      width="22"
+                      height="22"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M20.0104 0.125L1.85845 0.125C0.866598 0.125 0 0.838669 0 1.81887L0 20.0111C0 20.9967 0.866598 21.8749 1.85845 21.8749L20.0051 21.8749C21.0023 21.8749 21.7499 20.9908 21.7499 20.0111L21.7499 1.81887C21.7557 0.838669 21.0023 0.125 20.0104 0.125ZM6.74199 18.2546L3.62612 18.2546L3.62612 8.56669L6.74199 8.56669L6.74199 18.2546ZM5.29183 7.09371H5.2695C4.27231 7.09371 3.62661 6.3514 3.62661 5.42217C3.62661 4.47596 4.2893 3.75112 5.30883 3.75112C6.32835 3.75112 6.95221 4.47062 6.97454 5.42217C6.97406 6.3514 6.32835 7.09371 5.29183 7.09371ZM18.1296 18.2546L15.0138 18.2546L15.0138 12.9575C15.0138 11.6884 14.5603 10.8213 13.433 10.8213C12.5717 10.8213 12.062 11.4039 11.8353 11.9714C11.7503 12.1753 11.7275 12.453 11.7275 12.7366L11.7275 18.2546L8.61161 18.2546L8.61161 8.56669L11.7275 8.56669L11.7275 9.91489C12.1809 9.26919 12.8893 8.33997 14.5375 8.33997C16.5829 8.33997 18.1301 9.68817 18.1301 12.5948L18.1296 18.2546Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href="#"
+                    ><svg
+                      width="23"
+                      height="18"
+                      viewBox="0 0 23 18"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M22.2211 2.74428C21.4156 3.09399 20.5635 3.32438 19.6915 3.42818C20.6079 2.89196 21.297 2.04007 21.6297 1.03229C20.7633 1.53855 19.817 1.89377 18.8313 2.08273C18.4162 1.64804 17.9171 1.30223 17.3641 1.06633C16.8111 0.830423 16.2159 0.709346 15.6147 0.710457C13.1804 0.710457 11.2104 2.65041 11.2104 5.04183C11.2087 5.37446 11.2468 5.70611 11.324 6.02968C9.57849 5.94791 7.86923 5.50294 6.30563 4.72324C4.74202 3.94353 3.3585 2.84626 2.24358 1.50164C1.85244 2.16056 1.64558 2.91237 1.64463 3.6785C1.64463 5.1804 2.42877 6.50797 3.61281 7.28574C2.9113 7.2691 2.22416 7.08356 1.60974 6.74487V6.79851C1.60974 8.89938 3.1306 10.6471 5.14351 11.045C4.76499 11.1458 4.37493 11.1969 3.98318 11.1969C3.70521 11.1974 3.42787 11.1705 3.15521 11.1165C3.71479 12.8374 5.34346 14.089 7.27273 14.1247C5.70506 15.332 3.78038 15.9846 1.80119 15.9798C1.44987 15.9792 1.09889 15.9584 0.75 15.9172C2.76344 17.2019 5.1039 17.8815 7.4928 17.875C15.6053 17.875 20.0373 11.2729 20.0373 5.54693C20.0373 5.35919 20.0323 5.17146 20.0234 4.98819C20.8836 4.37673 21.6278 3.61686 22.2211 2.74428Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <a href="#"
+                    ><svg
+                      width="24"
+                      height="22"
+                      viewBox="0 0 24 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M23.2207 11.233C23.2207 5.28459 18.0714 0.461426 11.7207 0.461426C5.37003 0.461426 0.220703 5.28459 0.220703 11.233C0.220703 16.6092 4.42539 21.0654 9.92383 21.8743L9.92383 14.3476L7.00313 14.3476L7.00313 11.233L9.92383 11.233L9.92383 8.85988C9.92383 6.16074 11.6411 4.66859 14.2676 4.66859C15.526 4.66859 16.8423 4.87921 16.8423 4.87921L16.8423 7.53027L15.3915 7.53027C13.9637 7.53027 13.5171 8.36026 13.5171 9.21333L13.5171 11.233L16.7063 11.233L16.197 14.3476L13.5176 14.3476L13.5176 21.8752C19.016 21.0669 23.2207 16.6106 23.2207 11.233Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="border-1 border-top py-3">
+    <div class="container">
+      <div class="d-flex justify-content-between flex-wrap row-gap-3">
+        <p class="text-white fs-6 fw-medium mb-0">
+          Copyright © TransitFlow | Designed by VictorFlow - Powered by
+          Webflow.
+        </p>
+        <ul class="d-flex mb-0 ps-0 gap-3 flex-wrap">
+          <li>
+            <a href="#" class="d-inline-block fs-6 fw-normal text-white"
+              >Style Guide
+            </a>
+          </li>
+          <li>
+            <a href="#" class="d-inline-block fs-6 fw-normal text-white"
+              >Licenses</a
+            >
+          </li>
+          <li>
+            <a href="#" class="d-inline-block fs-6 fw-normal text-white"
+              >Changelog</a
+            >
+          </li>
+          <li>
+            <a href="#" class="d-inline-block fs-6 fw-normal text-white"
+              >Password</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>`;
+}
